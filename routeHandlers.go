61ec964968c3d38ffcbab94d01f4130508873722
szuhaydv <szuhaydv@gmail.com>
2024-09-18 21:37:56 +0200

    server returns HTML

--- routeHandlers.go
+++ routeHandlers.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "fmt"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
+
+type PageVariables struct {
+  Heading string
+  Content string
+}
+
+var tmpl1 = template.Must(template.ParseFiles("home.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received a request at my domain")
-	w.Write([]byte("Hello, welcome to my project!"))
+
+  data := PageVariables{
+    Heading: "Welcome to the Home Page",
+    Content: "This is a dynamically generated HTML response",
+  }
+
+  tmpl1.ExecuteTemplate(w, "home.html", data)
 }
 
 func featuresHandler(w http.ResponseWriter, r *http.Request) {
